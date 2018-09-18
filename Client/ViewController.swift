f79e890a4001607530324948a04574a62793a227
christianampe <christianampe.work@gmail.com>
2018-09-17 19:30:46 -0700

    Initial Commit

---
+++ Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Client
+//
+//  Created by Ampe on 9/17/18.
+//  Copyright © 2018 educrate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
