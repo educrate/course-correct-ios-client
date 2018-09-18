5bce18ebb46083b8fb356f91d8ba2bc3120d071a
christianampe <christianampe.work@gmail.com>
2018-09-17 22:08:57 -0700

    set up methods for table view implemented

--- Client/Modules/Calendar/View/View/CalendarView.swift
+++ Client/Modules/Calendar/View/View/CalendarView.swift
@@ -35,15 +35,46 @@ public class CalendarView: UIView {
         
         super.init(frame: frame)
         
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
+}
+
+private extension CalendarView {
+    func setUp() {
+        setUpTableView()
+    }
+    
+    // MARK: Table View Set Up
+    
+    func setUpTableView() {
+        initializeTableView()
+        styleTableView()
+        constrainTableView()
+    }
+    
+    func initializeTableView() {
         self.calendarTableView.delegate = self
         self.calendarTableView.dataSource = self
         
         self.calendarTableView.register(CalendarViewDayCell.self, forCellReuseIdentifier: "calendardaycell")
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    func styleTableView() {
         
-        super.init(coder: aDecoder)
+    }
+    
+    func constrainTableView() {
+        calendarTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarTableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        calendarTableView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        calendarTableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        calendarTableView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
     }
 }
 
