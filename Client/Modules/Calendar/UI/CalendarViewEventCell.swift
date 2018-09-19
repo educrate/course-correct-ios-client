51bbe6ac5a2fc3f583b847df00fe0ae806c5cfce
christianampe <christianampe.work@gmail.com>
2018-09-18 23:25:48 -0700

    began building the event cell

--- Client/Modules/Calendar/UI/CalendarViewEventCell.swift
+++ Client/Modules/Calendar/UI/CalendarViewEventCell.swift
@@ -12,11 +12,45 @@ class CalendarViewEventCell: UITableViewCell {
     
     // MARK: Views
     
-    private weak var title: UILabel!
-    private weak var detail: UILabel!
-    private weak var leftIcon: UIImageView!
-    private weak var rightIcon: UIImageView!
+    private weak var titleLabel: UILabel!
+    private weak var detailLabel: UILabel!
+    private weak var leftIconImageView: UIImageView!
+    private weak var rightIconImageView: UIImageView!
     
     
+    // MARK: Initializers
+    
+    override init(style: UITableViewCell.CellStyle,
+                  reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let tl = UILabel()
+        titleLabel = tl
+        
+        let dl = UILabel()
+        detailLabel = dl
+        
+        let lv = UIImageView()
+        leftIconImageView = lv
+        
+        let rv = UIImageView()
+        rightIconImageView = rv
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let tl = UILabel()
+        titleLabel = tl
+        
+        let dl = UILabel()
+        detailLabel = dl
+        
+        let lv = UIImageView()
+        leftIconImageView = lv
+        
+        let rv = UIImageView()
+        rightIconImageView = rv
+    }
 }
 
