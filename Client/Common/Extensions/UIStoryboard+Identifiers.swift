f684c10010ea343bd082754d20dbf5fe62395bea
christianampe <christianampe.work@gmail.com>
2018-10-14 22:27:17 -0700

    updated storybord initializable organization

--- Client/Common/Extensions/UIStoryboard+Identifiers.swift
+++ Client/Common/Extensions/UIStoryboard+Identifiers.swift
@@ -48,3 +48,18 @@ extension UIStoryboard {
         case uiInlinePicker = "UIInlinePicker"
     }
 }
+
+// MARK: - Convenience Initializers
+extension UIStoryboard {
+    convenience init(storyboard: Storyboard, bundle: Bundle? = nil) {
+        self.init(name: storyboard.rawValue, bundle: bundle)
+    }
+}
+
+
+// MARK: - Class Functions
+extension UIStoryboard {
+    class func storyboard(_ storyboard: Storyboard, bundle: Bundle? = nil) -> UIStoryboard {
+        return UIStoryboard(name: storyboard.rawValue, bundle: bundle)
+    }
+}
--- Client/Modules/Utility/Storyboard/StoryboardIdentifiable.swift
+++ Client/Modules/Utility/Storyboard/StoryboardIdentifiable.swift
@@ -8,36 +8,103 @@
 
 import UIKit
 
-
 // MARK: - Protocol Requiring an Identifier
 protocol StoryboardIdentifiable {
     static var storyboardIdentifier: String { get }
 }
 
-// MARK: - Extract Identifier From View Controller
+
+// MARK: - View Controller
+
+// MARK: Conforms UIViewController to StoryboardIdentifiable
+extension UIViewController: StoryboardIdentifiable {}
+
+// MARK: Extract Identifier From View Controller
 extension StoryboardIdentifiable where Self: UIViewController {
     static var storyboardIdentifier: String {
         return String(describing: self)
     }
 }
 
-// MARK: - Extract Identifier From Reusable Cell
+// MARK: View Controller Instantiation from Generics
+extension UIStoryboard {
+    func instantiateViewController<T: UIViewController>() -> T {
+        guard let viewController = self.instantiateViewController(withIdentifier: T.storyboardIdentifier) as? T else {
+            fatalError("Couldn't instantiate view controller with identifier \(T.storyboardIdentifier) ")
+        }
+        
+        return viewController
+    }
+}
+
+
+// MARK: - Table View Cell
+
+// MARK: Conforms UITableViewCell to StoryboardIdentifiable
+extension UITableViewCell: StoryboardIdentifiable {}
+
+// MARK: Extract Identifier From Reusable Cell
 extension StoryboardIdentifiable where Self: UITableViewCell {
     static var storyboardIdentifier: String {
         return String(describing: self)
     }
 }
 
-// MARK: - Extract Identifier From Reusable Cell
+// MARK: Table View Cell Instantiation from Generics
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.storyboardIdentifier, for: indexPath as IndexPath) as? T else {
+            fatalError("Cannot dequeue: \(T.self) with identifier: \(T.storyboardIdentifier)")
+        }
+        
+        return cell
+    }
+}
+
+
+// MARK: - Collection View
+
+// MARK: Conforms UICollectionViewCell to StoryboardIdentifiable
+extension UICollectionViewCell: StoryboardIdentifiable {}
+
+// MARK: Extract Identifier From Reusable Cell
 extension StoryboardIdentifiable where Self: UICollectionViewCell {
     static var storyboardIdentifier: String {
         return String(describing: self)
     }
 }
 
-// MARK: - Extract Identifier From Reusable Cell
+// MARK: Collection View Cell Instantiation from Generics
+extension UICollectionView {
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.storyboardIdentifier, for: indexPath as IndexPath) as? T else {
+            fatalError("Cannot dequeue: \(T.self) with identifier: \(T.storyboardIdentifier)")
+        }
+        
+        return cell
+    }
+}
+
+
+// MARK: - Segues
+
+// MARK: Conforms UIStoryboardSegue to StoryboardIdentifiable
+extension UIStoryboardSegue: StoryboardIdentifiable {}
+
+// MARK: Extract Identifier From Reusable Cell
 extension StoryboardIdentifiable where Self: UIStoryboardSegue {
     static var storyboardIdentifier: String {
         return String(describing: self)
+    }
+}
+
+// MARK: View Controller Validation from Generics
+extension UIStoryboardSegue {
+    func viewController<T: UIViewController>() -> T {
+        guard let viewController = destination as? T else {
+            fatalError("Cannot segue to: \(T.self) with identifier: \(T.storyboardIdentifier)")
+        }
+        
+        return viewController
     }
 }
