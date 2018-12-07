3bb2bd770645d130f0004c631bcad2fe6b0ab16f
christianampe <christianampe.work@gmail.com>
2018-12-06 16:24:08 -0800

    updated pseudo implementation to be more logical

--- Source/Application/Entry/App Coordinator/Main Coordinator/Calendar/CalendarViewController.swift
+++ Source/Application/Entry/App Coordinator/Main Coordinator/Calendar/CalendarViewController.swift
@@ -38,9 +38,9 @@ extension CalendarViewController: UICalendarViewDataSource {
     func calendarView(_ calendarView: UICalendarView,
                       dayForItemAt indexPath: IndexPath) -> UICalendarViewDay {
         
-        return UICalendarViewDay(date: UICalendarViewDayDescription(day: 1,
-                                                                    month: 1,
-                                                                    year: 2018,
+        return UICalendarViewDay(date: UICalendarViewDayDescription(day: indexPath.row + 1,
+                                                                    month: indexPath.section + 1,
+                                                                    year: 2018 + (indexPath.section / 12),
                                                                     dayNameShort: "Fri",
                                                                     monthNameShort: "Jan",
                                                                     monthName: "January"),
