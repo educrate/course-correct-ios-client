e345e15d16f3daa9251e541542809f9cdd7ca1b6
christianampe <christianampe.work@gmail.com>
2018-12-09 18:22:54 -0800

    added documentation to the xib identifiable file

--- Source/Components/Utilities/Interface Builder/XIB/XIBIdentifiable.swift
+++ Source/Components/Utilities/Interface Builder/XIB/XIBIdentifiable.swift
@@ -10,13 +10,20 @@ import UIKit
 
 // MARK: - Protocol Requiring an Identifier
 protocol XIBIdentifiable {
+    
+    /// The identifier used to describe
+    /// the associated xib file.
     static var xibIdentifier: String { get }
 }
 
+// MARK: - XIB Identifiable Conformation
 extension XIBView: XIBIdentifiable {}
 
-// MARK: Extract Identifier From View Controller
+// MARK: - Extract Identifier From View Controller
 extension XIBIdentifiable where Self: XIBView {
+    
+    /// Default value set from the Swift generated
+    /// description of the view.
     static var xibIdentifier: String {
         return String(describing: self)
     }
