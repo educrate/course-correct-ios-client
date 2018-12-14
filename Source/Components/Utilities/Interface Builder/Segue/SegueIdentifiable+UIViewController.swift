27d23ca0416219bb90b7f7764eeb3edeec82fdf2
christianampe <christianampe.work@gmail.com>
2018-12-14 11:53:44 -0800

    further updated documentation

--- Source/Components/Utilities/Interface Builder/Segue/SegueIdentifiable+UIViewController.swift
+++ Source/Components/Utilities/Interface Builder/Segue/SegueIdentifiable+UIViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 extension SegueIdentifiable where Self: UIViewController, Segue.RawValue == String {
+    
+    /// Method used for extracting the segue case from the storyboard's UIStoryboardSegue.
+    ///
+    /// - Parameter segue: The UIStoryboardSegue passed from the storyboard.
+    /// - Returns: Returns the enum case for which the UIStoryboardSegue is associated.  If there is no associated case, this method throws an exception.
     func segueCase(for segue: UIStoryboardSegue) -> Segue {
         guard let identifier = segue.identifier, let identifierCase = Segue(rawValue: identifier) else {
             fatalError("Could not map segue identifier - \(String(describing: segue.identifier)) - to segue case")
